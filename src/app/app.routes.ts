ea82d918ecd2cf13225dde61a089d7f4b4cfd165
TouchinBatge75 <elmemes512@hotmail.com>
2025-05-22 03:14:23 -0600

    Se creo el componente para subir viajes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { PostComponent } from './components/post/post.component';
 
 
 export const routes: Routes = [
@@ -9,4 +10,5 @@ export const routes: Routes = [
       { path: 'login', component: LoginComponent}, 
       { path: 'register', component: RegisterComponent },
       { path: 'home', component: HomeComponent },
+      { path: 'post', component: PostComponent },
 ];
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.css'
+  styleUrls: ['./post.component.css']
 })
 export class PostComponent {
+  viajeForm: FormGroup;
+  viajePublicado = false;
+  viajeData: any = null;
 
+  constructor(private fb: FormBuilder) {
+    this.viajeForm = this.fb.group({
+      origen: ['', Validators.required],
+      destino: ['', Validators.required],
+      fecha: ['', Validators.required],
+      hora: ['', Validators.required],
+      asientosDisponibles: [1, [Validators.required, Validators.min(1)]],
+      costoPorPersona: [0, [Validators.required, Validators.min(1)]],
+      notas: ['']
+    });
+  }
+
+  publicarViaje() {
+    if (this.viajeForm.invalid) {
+      this.viajeForm.markAllAsTouched();
+      return;
+    }
+
+    this.viajeData = this.viajeForm.value;
+    this.viajePublicado = true;
+
+    // Aquí podrías mandar los datos a tu backend
+    console.log('Viaje publicado:', this.viajeData);
+  }
 }
